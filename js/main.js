36901a25130e027cf9ad87c8957e5c49bbe0aa58
Margarete Koenen <margarete.koenen@loudfeed.com>
2016-03-24 16:15:44 -0400

    disable hydration to make the app work

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ function init(){
 }
 
 function initializeMap() {
-    echo "trying to build map";
     var mymap = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
