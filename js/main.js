1f52b4d41ed8a90e2c5c13a97176504b879f0dd4
Margarete Koenen <margarete.koenen@loudfeed.com>
2016-03-24 17:27:08 -0400

    change access token

--- js/main.js
+++ js/main.js
@@ -19,7 +19,8 @@ function initializeMap() {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
         id: 'mkoenen.pgbhd1mm',
-        accessToken: 'pk.eyJ1IjoibWtvZW5lbiIsImEiOiJjaW02aWY3cmkwMmhjdHFtNjA4ZWxpdHZjIn0.rALCihDQ0mzv1iYkoXCEwA'
+        accessToken: 'pk.eyJ1IjoibWtvZW5lbiIsImEiOiJjaW02aTl4MHYwMmJxdTRtNnF2czM0NTByIn0.KU18qMwQrcVekSMRvV-7uw'
+        
     }).addTo(mymap);
 }
 
