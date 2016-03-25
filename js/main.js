02123817c99ee020208177380b2c5f9e638e8c4f
Margarete Koenen <margarete.koenen@loudfeed.com>
2016-03-25 19:57:09 -0400

    increase possible zoom level

--- js/main.js
+++ js/main.js
@@ -17,7 +17,8 @@ function initializeMap() {
     var map = L.map('map');
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-        
+        maxZoom: 21,
+        maxNativeZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
             '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
             'Imagery © <a href="http://mapbox.com">Mapbox</a>',
